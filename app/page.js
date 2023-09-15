6a4967569db41b97647884d610ffbad7292d912e
Prepsld <119632733+Prepsld@users.noreply.github.com>
2023-09-15 13:03:45 -0600

    adds week page 2 and links

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import Link from 'next/link';
+
 export default function Page 
 () {
   return ( <main> 
     <h1 className="text-4xl m-8">Hello world!</h1>
    <p>This is a paragraph, written by David Prepsl and following the teachers demonstration</p> 
    <p>This is a second paragraph to demonstrate githubs checkpoints.</p>
+   <Link href="/week2">Week 2</Link>
    </main> );
 }
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function week2() {
+    return (
+        <main>
+            <h1>Community Events</h1>
+            <Link href="/">Home</Link>
+        </main>
+    );
+}
