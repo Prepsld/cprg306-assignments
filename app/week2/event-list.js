11fb64ac9c2bfe38e06f28a6ca1986ae85b7ffae
Prepsld <119632733+Prepsld@users.noreply.github.com>
2023-09-15 13:09:21 -0600

    adds eventlist and displays on page 2

---
+++ app/week2/event-list.js
@@ -0,0 +1,11 @@
+export default function EventList() {
+    return (
+        <ul>
+            <li>
+                <h2>Community picnic</h2>
+                <p>Date: September 16th, 2023</p>
+                <p>Location: Central Park</p>
+            </li>
+        </ul>
+    )
+}
--- app/week2/page.js
+++ app/week2/page.js
@@ -1,10 +1,14 @@
 import Link from 'next/link';
-
+import EventList from './event-list';
 export default function week2() {
     return (
         <main>
             <h1>Community Events</h1>
-            <Link href="/">Home</Link>
+
+            <p>
+                <Link href="/">Home</Link>
+            </p>
+            <EventList />
         </main>
     );
 }
