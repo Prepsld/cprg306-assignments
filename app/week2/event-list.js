de716b26864051d0f567698e89e4999eafde64e5
Prepsld <119632733+Prepsld@users.noreply.github.com>
2023-09-15 13:10:34 -0600

    adds new list item

--- app/week2/event-list.js
+++ app/week2/event-list.js
@@ -6,6 +6,11 @@ export default function EventList() {
                 <p>Date: September 16th, 2023</p>
                 <p>Location: Central Park</p>
             </li>
+            <li>
+                <h2>Community Clean up</h2>
+                <p>Date: April 10th, 2024</p>
+                <p>Location: Central Park</p>
+            </li>
         </ul>
     )
 }
