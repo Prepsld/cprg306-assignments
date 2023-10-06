1287f64de721c7d934fd4e72d6da531298a031bc
Prepsld <119632733+Prepsld@users.noreply.github.com>
2023-10-06 12:07:55 -0600

    changes text format

--- app/week3/item.js
+++ app/week3/item.js
@@ -2,7 +2,7 @@ export default function Item ({name, quantity, category}) {
     return (
     <div>
         <ol>
-            <li className="border border-red-800 bg-yellow-300 w-full max-w-xs m-4 p-2" >
+            <li className="text-red-300 bg-slate-800 border border-blue-800" >
                 <p className="text-red-800">Name: {name}</p>
                 <p>Quantity: {quantity}</p>
                 <p>Category: {category}</p>
