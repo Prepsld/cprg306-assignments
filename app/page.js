346227c8be4c61b6373ca1dcfabfb306207d3c79
Prepsld <119632733+Prepsld@users.noreply.github.com>
2023-10-06 19:27:02 -0600

    actually fixes issues

--- app/page.js
+++ app/page.js
@@ -4,23 +4,14 @@ import StudentInfo from './StudentInfo';
 export default function Page() {
   return (
     <main className="bg-gray-900 text-white p-8">
-      {/* Page title */}
       <h1 className="text-4xl mb-8 text-center">CPRG 306: Web Development 2 - Assignments</h1>
-
-      {/* First paragraph */}
       <p className="text-center">
-        This is a paragraph, written by David Prepsl and following the teacher's demonstration
+        This is a paragraph, written by David Prepsl and following the teachers demonstration
       </p>
-
-      {/* Second paragraph */}
       <p className="text-center">
-        This is a second paragraph to demonstrate GitHub's checkpoints.
+        This is a second paragraph to demonstrate GitHubs checkpoints.
       </p>
-
-      {/* Render the StudentInfo component */}
       <StudentInfo />
-
-      {/* Navigation links */}
       <div className="flex justify-center">
         <Link href="/week2" className="text-blue-400 mx-2 hover:text-blue-600">
           Week 2
